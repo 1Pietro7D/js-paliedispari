c4a505a0a445310ec815b0dbbf8f800e6848b9b5
PietroDanieli <1pie7d@gmail.com>
2022-09-01 03:59:10 +0200

    two solution for palindrome

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,36 @@ console.log("miao");
 // Palindroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+// SOLUTION 1 PALINDROME
+alert(returnPalindrome(prompt("inserisci una parola")));
+
+function returnPalindrome(word) {
+  let reverseWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+
+  if (word == reverseWord) {
+    return "palindromo";
+  } else {
+    return "non è un palindromo";
+  }
+}
+// Manca il controllo di inserire almeno una lettera altrimenti si rompe tutto
+// SOLUTION 2 PALINDROME
+const textToAnalize = document.getElementById("palindrome-text").value;
+
+function palindrome(word1) {
+  const reversed = word1.split("").reverse().join("");
+
+  return word1 === reversed ? "palindroma" : "non è palindroma";
+}
+const buttonPalindrome = document.getElementById("button-palindrome");
+buttonPalindrome.addEventListener("click", function () {
+  const result = document.createElement("span");
+  result.innerText = palindrome(textToAnalize);
+  document.getElementById("palindrome-contenitor").appendChild(result);
+});
 
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
